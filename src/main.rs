875dcfc523b762bc71846a62b340abaef52b9c22
wappon28dev <wappon_28_dev@ruru.be>
2025-02-10 18:23:04 +0900

    🚧 並列書き出し

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,57 @@ use std::iter::zip;
 
 use modules::{file, video};
 
+fn get_style(is_done: bool) -> ProgressStyle {
+    ProgressStyle::with_template(&format!(
+        "\n{} -> {}\n  {}\n  {}{} {} {} | {}{} | {}",
+        style("{spinner}").blue(),
+        style("{prefix}"),
+        "{bar:40.cyan/blue}",
+        "{pos:>3}",
+        style("/{len:>3}").dim(),
+        style("[fr]").dim(),
+        style(format!("({})", style("{percent:>3}%").for_stdout())).dim(),
+        "{elapsed_precise}",
+        if is_done {
+            style("/{elapsed_precise}").dim()
+        } else {
+            style("/{duration_precise}").dim()
+        },
+        style("{msg}")
+    ))
+    .unwrap()
+    .progress_chars("=>-")
+}
+
 async fn process(input_path: &str, crf: u32, pb: ProgressBar) -> Result<()> {
     let output_path = format!("out/2--crf-{}.mp4", crf);
 
-    video::process(input_path, &output_path, crf, pb.clone())
-        .await
-        .with_context(|| format!("CRF {} でのエンコードに失敗しました.", crf))?;
+    video::process(
+        video::VideoProcessParams {
+            input_path: input_path.to_string(),
+            output_path: output_path.clone(),
+            video_config: video::VideoConfig {
+                res: video::VideoRes::Other(-1, -1).into(),
+                fps: None,
+                crf,
+                has_audio: true,
+            },
+        },
+        pb.clone(),
+    )
+    .await
+    .with_context(|| format!("CRF {} でのエンコードに失敗しました.", crf))?;
 
     let output_size =
         file::calc_size(&output_path).context("出力動画のサイズの取得に失敗しました.")?;
     let output_size_str = format_size(output_size, DECIMAL);
 
-    pb.finish_with_message(format!("✓ エンコード完了: {}", output_size_str));
+    pb.set_style(get_style(true));
+    pb.finish_with_message(format!(
+        "{}: {}",
+        style("✓ エンコード完了").green(),
+        style(output_size_str).green().bright()
+    ));
 
     Ok(())
 }
@@ -31,11 +70,7 @@ async fn main() -> Result<()> {
     let crf_iter = (20..40).step_by(10);
 
     let progress = MultiProgress::new();
-    let spinner_style = ProgressStyle::with_template(
-        "{spinner:.green.dim} {bar:40.cyan/blue} {pos:>3}/{len:>3.gray} [fr] ({percent}%) - {elapsed_precise}/{duration_precise} - {msg}"
-    )
-    .unwrap()
-    .progress_chars("█▓▒░");
+    let spinner_style = get_style(false);
 
     let tasks = crf_iter.clone().map(|crf| {
         let pb = progress.add(ProgressBar::no_length());
@@ -46,7 +81,14 @@ async fn main() -> Result<()> {
     });
 
     let origin_size = file::calc_size(input_path).context("元動画のサイズの取得に失敗しました.")?;
-    println!("元動画のサイズ: {}", format_size(origin_size, DECIMAL));
+    println!(
+        "{}",
+        style(format!(
+            "元動画のサイズ: {}",
+            format_size(origin_size, DECIMAL)
+        ))
+        .dim()
+    );
 
     let binding = futures::future::join_all(tasks).await;
     let results = binding.iter().map(|r| match r {
@@ -55,6 +97,7 @@ async fn main() -> Result<()> {
         Err(e) => Err(anyhow!("{:?}", e)),
     });
 
+    println!();
     println!();
     results.clone().all(|r| r.is_ok()).then(|| {
         println!("{}", style("✓ すべて正常にエンコードしました！").green());
--- src/modules/video.rs
+++ src/modules/video.rs
@@ -1,4 +1,6 @@
-use anyhow::{bail, Result};
+use std::ffi::OsStr;
+
+use anyhow::{anyhow, bail, Context, Result};
 use ffmpeg_sidecar::{
     command::FfmpegCommand,
     event::{FfmpegDuration, FfmpegEvent, FfmpegProgress, LogLevel, StreamTypeSpecificData},
@@ -6,10 +8,112 @@ use ffmpeg_sidecar::{
 use humansize::ToF64;
 use indicatif::ProgressBar;
 
-pub async fn process(input_path: &str, output_path: &str, crf: u32, pb: ProgressBar) -> Result<()> {
+pub enum VideoRes {
+    R240p,
+    R360p,
+    R480p,
+    R720p,
+    R1080p,
+    R1440p,
+    R2160p,
+    R4320p,
+    Other(i32, i32),
+}
+
+enum ToStrError {
+    BothAreDynamicValue,
+}
+
+impl VideoRes {
+    pub fn to_wh(&self) -> (i32, i32) {
+        match self {
+            VideoRes::R240p => (426, 240),
+            VideoRes::R360p => (640, 360),
+            VideoRes::R480p => (854, 480),
+            VideoRes::R720p => (1280, 720),
+            VideoRes::R1080p => (1920, 1080),
+            VideoRes::R1440p => (2560, 1440),
+            VideoRes::R2160p => (3840, 2160),
+            VideoRes::R4320p => (7680, 4320),
+            VideoRes::Other(w, h) => (*w, *h),
+        }
+    }
+
+    pub fn to_name(&self) -> &str {
+        match self {
+            VideoRes::R240p => "240p (SD)",
+            VideoRes::R360p => "360p (SD)",
+            VideoRes::R480p => "480p (SD)",
+            VideoRes::R720p => "720p (HD)",
+            VideoRes::R1080p => "1080p (FHD)",
+            VideoRes::R1440p => "1440p (QHD)",
+            VideoRes::R2160p => "2160p (4K)",
+            VideoRes::R4320p => "4320p (8K)",
+            VideoRes::Other(_, _) => "その他",
+        }
+    }
+
+    pub fn from_wh(width: i32, height: i32) -> Self {
+        match (width, height) {
+            (426, 240) => VideoRes::R240p,
+            (640, 360) => VideoRes::R360p,
+            (854, 480) => VideoRes::R480p,
+            (1280, 720) => VideoRes::R720p,
+            (1920, 1080) => VideoRes::R1080p,
+            (2560, 1440) => VideoRes::R1440p,
+            (3840, 2160) => VideoRes::R2160p,
+            (7680, 4320) => VideoRes::R4320p,
+            _ => VideoRes::Other(width, height),
+        }
+    }
+
+    pub fn to_args(&self) -> Result<String, ToStrError> {
+        let (width, height) = self.to_wh();
+
+        match (width, height) {
+            (-1, -1) => Err(ToStrError::BothAreDynamicValue),
+            (_, -1) | (-1, _) => Ok(format!("-vf scale={}:{}", width, height)),
+            _ => Ok(format!("-s {}x{}", width, height)),
+        }
+    }
+}
+
+pub struct VideoConfig {
+    pub res: Option<VideoRes>,
+    pub fps: Option<f64>,
+    pub crf: u32,
+    pub has_audio: bool,
+}
+
+pub struct VideoProcessParams {
+    pub input_path: String,
+    pub output_path: String,
+    pub video_config: VideoConfig,
+}
+
+pub async fn process(params: VideoProcessParams, pb: ProgressBar) -> Result<()> {
+    let VideoProcessParams {
+        input_path,
+        output_path,
+        video_config,
+    } = params;
+
+    let arg = match video_config.res {
+        Some(res) => res.to_args().map_err(|e| {
+            match e {
+                ToStrError::BothAreDynamicValue => {
+                    anyhow!("高さと幅のどちらも動的な値 (`-1`) に設定されています. どちらか一方を固定値にしてください.")
+                },
+            }
+        }).context("映像の高さと幅の指定に失敗しました.")?,
+        None => "".to_string(),
+    };
+    let arg_os_str: Vec<&OsStr> = arg.split_whitespace().map(OsStr::new).collect();
+
     let mut runner = FfmpegCommand::new()
         .input(input_path)
-        .crf(crf)
+        .crf(video_config.crf)
+        .args(arg_os_str)
         .output(output_path)
         .overwrite()
         .spawn()
